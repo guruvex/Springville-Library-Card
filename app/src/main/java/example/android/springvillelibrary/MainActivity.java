73f2c3371a126a85281b20ae1181396fbe9593cb
james heath <guruvex@gmail.com>
2018-04-12 18:31:57 -0600

    work in progress

--- app/src/main/java/example/android/springvillelibrary/MainActivity.java
+++ app/src/main/java/example/android/springvillelibrary/MainActivity.java
@@ -2,6 +2,7 @@ package example.android.springvillelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showTime(View view) {
+    }
+
+    public void showWeb(View view) {
+    }
+
+    public void showPhone(View view) {
     }
 }

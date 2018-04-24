30e87682ac4befe8935715f28c5d8b00352db024
james heath <guruvex@gmail.com>
2018-04-24 11:39:10 -0600

    added the buttons to display the info and other small fix's

--- app/src/main/java/example/android/springvillelibrary/MainActivity.java
+++ app/src/main/java/example/android/springvillelibrary/MainActivity.java
@@ -3,6 +3,8 @@ package example.android.springvillelibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +15,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showTime(View view) {
+
+        TextView infoText = findViewById(R.id.two);
+        String screenTextPart1 = getResources().getString(R.string.openDaysOne);
+        String screenTextPart2 = getResources().getString(R.string.openDaysTwo);
+        String screenTextPart3 = getResources().getString(R.string.openDaysThree);
+
+        String screenText = screenTextPart1 + "\n" + screenTextPart2 + "\n" +  screenTextPart3;
+        infoText.setText(screenText);
     }
 
     public void showWeb(View view) {
+        TextView infoText = findViewById(R.id.two);
+        String screenText = getResources().getString(R.string.webSite);
+        infoText.setText(screenText);
     }
 
     public void showPhone(View view) {
+        TextView infoText = findViewById(R.id.two);
+        String screenText = getResources().getString(R.string.phone);
+        infoText.setText(screenText);
+    }
+
+    public void showAddress(View view) {
+        TextView infoText = findViewById(R.id.two);
+        String screenText = getResources().getString(R.string.address);
+        infoText.setText(screenText);
     }
 }
